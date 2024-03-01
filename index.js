83c2c314558a0a97a21fe5ed09d1c58b89bd6a73
niaz abir <niazabir55@gmail.com>
2024-03-01 16:19:31 +0600

    adding

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 // app.use(cors(Credential:true));
 app.use(express.json());
 
